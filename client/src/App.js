708fdf6634215ba81eaf021ff7cde76ca94ed668
Gigantorz <69411983+Gigantorz@users.noreply.github.com>
2022-09-10 20:54:17 -0600

    Trying to get storing storing right from app.js into firebase

--- client/src/App.js
+++ client/src/App.js
@@ -93,7 +93,24 @@ function ChatRoom() {
   const query = messagesRef.orderBy('createdAt').limit(25); // order by the time and limit by 25
   // Why are we limiting by 25?
 
-  const [messages] useCollectionData(query, {idField: 'id'});
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const [formValue, setFormaValue] = useState('');
+
+return (<>
+  <main>
+    {messages && messages.map(msg => 
+      <ChatMessage key={msg.id} message={msg} />)}
+
+    <span ref={dummy}></span>
+  </main>
+
+    <form onSubmit={sendMessage}>
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+
+      <button type = "submit">🕊</button>
+    </form>
+  </>)
 }
 
 function ChatMessage(props) {
